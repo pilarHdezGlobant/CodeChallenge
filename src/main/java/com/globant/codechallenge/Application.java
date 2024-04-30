988e5d942b5e0d9234ddb0ca4aa9e21e7b9b73fe
mariadhernand <mariadhernand@expediagroup.com>
2024-04-30 13:24:15 -0600

    Solution: Print Numbers With No Loops

---
+++ src/main/java/com/globant/codechallenge/Application.java
@@ -0,0 +1,8 @@
+package com.globant.codechallenge;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Print Numbers With No Loops");
+        PrintNumbersWithNoLoops.printNumbersWithNoLoops(0, 10);
+    }
+}
